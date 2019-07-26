ece3d082a69dc9287d75f831fb439ae603da3c8b
Pablo <50279292+pabloolmedo@users.noreply.github.com>
2019-07-25 23:09:32 -0300

    First Commit

---
+++ VSapp/VSapp/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnSumar = New System.Windows.Forms.Button()
+        Me.dtpFechaInicio = New System.Windows.Forms.DateTimePicker()
+        Me.dtpFechaFinal = New System.Windows.Forms.DateTimePicker()
+        Me.btnCalcularFecha = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(179, 42)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(332, 42)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(490, 42)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 2
+        '
+        'btnSumar
+        '
+        Me.btnSumar.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnSumar.Location = New System.Drawing.Point(343, 94)
+        Me.btnSumar.Name = "btnSumar"
+        Me.btnSumar.Size = New System.Drawing.Size(75, 23)
+        Me.btnSumar.TabIndex = 3
+        Me.btnSumar.Text = "SUMAR"
+        Me.btnSumar.UseVisualStyleBackColor = False
+        '
+        'dtpFechaInicio
+        '
+        Me.dtpFechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaInicio.Location = New System.Drawing.Point(79, 250)
+        Me.dtpFechaInicio.Name = "dtpFechaInicio"
+        Me.dtpFechaInicio.ShowUpDown = True
+        Me.dtpFechaInicio.Size = New System.Drawing.Size(200, 20)
+        Me.dtpFechaInicio.TabIndex = 4
+        '
+        'dtpFechaFinal
+        '
+        Me.dtpFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpFechaFinal.Location = New System.Drawing.Point(490, 249)
+        Me.dtpFechaFinal.Name = "dtpFechaFinal"
+        Me.dtpFechaFinal.ShowUpDown = True
+        Me.dtpFechaFinal.Size = New System.Drawing.Size(200, 20)
+        Me.dtpFechaFinal.TabIndex = 5
+        '
+        'btnCalcularFecha
+        '
+        Me.btnCalcularFecha.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnCalcularFecha.Location = New System.Drawing.Point(343, 296)
+        Me.btnCalcularFecha.Name = "btnCalcularFecha"
+        Me.btnCalcularFecha.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcularFecha.TabIndex = 6
+        Me.btnCalcularFecha.Text = "CALCULAR"
+        Me.btnCalcularFecha.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(340, 256)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(301, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "+"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(458, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "="
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalcularFecha)
+        Me.Controls.Add(Me.dtpFechaFinal)
+        Me.Controls.Add(Me.dtpFechaInicio)
+        Me.Controls.Add(Me.btnSumar)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnSumar As Button
+    Friend WithEvents dtpFechaInicio As DateTimePicker
+    Friend WithEvents dtpFechaFinal As DateTimePicker
+    Friend WithEvents btnCalcularFecha As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ VSapp/VSapp/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtNum1.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtNum1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNum1.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub txtNum2_TextChanged(sender As Object, e As EventArgs) Handles txtNum2.TextChanged
+
+    End Sub
+
+    Private Sub btnSumar_Click(sender As Object, e As EventArgs) Handles btnSumar.Click
+        Dim sumaTotal As Int16
+
+
+        sumaTotal = Convert.ToInt16(txtNum1.Text) + Convert.ToInt16(txtNum2.Text)
+        txtTotal.Text = sumaTotal
+        MessageBox.Show(sumaTotal)
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub txtNum2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNum2.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Dim mostrarSuma As Int16
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcularFecha.Click
+        Dim fechaInicio As Date = dtpFechaInicio.Value.ToShortDateString
+
+
+        Dim fechaFinal As Date = dtpFechaFinal.Value.ToShortDateString
+
+
+        Dim diferenciaDias As String = DateDiff(DateInterval.Day, fechaInicio, fechaFinal)
+
+        MessageBox.Show(diferenciaDias)
+        Label1.Visible = True
+        Label1.Text = "Dieferencia igual a :"
+        Label1.Text = Label1.Text + diferenciaDias
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub DateTime1_ValueChanged(sender As Object, e As EventArgs) Handles dtpFechaInicio.ValueChanged
+
+    End Sub
+End Class
